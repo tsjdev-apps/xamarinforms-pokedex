eb7bd4ff170ff7370984e446b72597240a3fcb47
Sebastian Jensen <apps@tsjdev-apps.de>
2022-01-27 20:34:21 +0100

    Add part 4 to the repository.

---
+++ Pokedex-Part04/Pokedex/Pokedex/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Pokedex.Init;
+using Pokedex.Resources;
+using Pokedex.Views;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Forms;
+
+namespace Pokedex
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            LocalizationResourceManager.Current.Init(AppResources.ResourceManager);
+
+            Bootstrapper.Init();
+
+            MainPage = new NavigationPage(new OverviewPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Pokedex-Part04/Pokedex/Pokedex/Common/ImageResourceExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pokedex.Common
+{
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+
+            return ImageSource.FromResource($"Pokedex.Assets.{Source}");
+        }
+    }
+}
